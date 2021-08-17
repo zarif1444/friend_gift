1ade2e3ef703fb993634ee30115c49c00b837b8b
Brad Traversy <polieumme@gmail.com>
2021-08-17 23:05:01 +0600

    for my 3 best friend

---
+++ main.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os
+
+fh = open("friend.txt","r")
+myText = fh.read().replace("\n", " ")
+
+language = 'en'
+
+output = gTTS(text=myText, lang=language, slow=False)
+
+output.save("output.mp3")
+fh.close()
+os.system("start output.mp3")
